e04cc5389a49ba8836c3c0a9ebcacc001c6f0abc
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-02-21 19:41:27 +0530

    added navbar code to navber component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect } from 'react'
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import Main from './components/Main'
 import WhatDoIDo from './components/WhatDoIDo'
 import AboutMe from './components/AboutMe'
@@ -22,18 +22,7 @@ function App() {
   }, [])
   return (
     <div className="w-full">
-      {/* <Navbar /> */}
-      <div className='nav-main sticky top-0 z-10'>
-        < nav className='nav absolute px-10 py-5 flex items-center justify-between' >
-          <div className="logo-sec">
-            <span className='c-200 logo text-4xl font-semibold'>S</span>
-          </div>
-          <div className="menu-sec">
-            <div className='hamburger'></div>
-            <div className='hamburger'></div>
-          </div>
-        </nav >
-      </div>
+      <Navbar />
       <Main />
       <div className='b-100'>
         <WhatDoIDo />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <>dslfk</>
+        <div className='nav-main sticky top-0 z-10'>
+            < nav className='nav absolute px-10 py-5 flex items-center justify-between' >
+                <div className="logo-sec">
+                    <span className='c-200 logo text-4xl font-semibold'>S</span>
+                </div>
+                <div className="menu-sec">
+                    <div className='hamburger'></div>
+                    <div className='hamburger'></div>
+                </div>
+            </nav >
+        </div>
     )
 }
