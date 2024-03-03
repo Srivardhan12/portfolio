74e186b7a7112078499c59a7ede83b8ea3fb2c89
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-03-03 19:34:25 +0530

    Finished project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import codefeImg from '../assests/images/codefe-5.jpg'
 
 export default function Projects() {
     return (
-        <div>
-            {/* Projects */}
+        <div className='w-11/12 m-auto px-10 h-screen'>
+            <h2 className='c-100 px-8 pt-20 uppercase text-8xl font-medium'>Projects</h2>
+            <div className="px-8 pt-10 flex">
+                <div>
+                    <img src={codefeImg} className='w-11/12' alt="ProjectImg" />
+                </div>
+                <div>
+                    <h3 className='c-100 uppercase text-5xl font-medium'>CodeFe</h3>
+                    <p className='c-200 text-3xl font-medium py-5'>CodeFE offers an immersive experience filled with exciting challenges and a playground to pratice your front-end skills.</p>
+                </div>
+            </div>
         </div>
     )
 }
