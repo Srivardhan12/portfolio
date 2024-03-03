f8790bb5f576b8fe904368937184dc324417cc52
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-03-03 19:39:48 +0530

    Changed font style of main page from font-medium to font-bold

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,8 @@ export default function Main() {
     return (
         <div className='landing b-200 flex justify-center items-center h-screen w-11/12 m-auto flex-col relative'>
             <div className='box'>
-                <span className='c-100 uppercase text-8xl font-medium text-start'>Hello there, I'm</span>
-                <h1 className='c-100 pt-10 uppercase text-8xl font-medium text-end'>Srivardhan</h1>
+                <span className='c-100 uppercase text-8xl font-semibold text-start'>Hello there, I'm</span>
+                <h1 className='c-100 pt-10 uppercase text-8xl font-semibold text-end'>Srivardhan</h1>
                 {/* <div>
                     <p className='c-300 pt-3 uppercase  font-medium text-end'>STUDENT AND A WEB DEVELOPER</p>
                 </div> */}
