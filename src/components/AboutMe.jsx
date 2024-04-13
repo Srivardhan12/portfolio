84a16a07d79243c77b79492d7a5263a060e89a18
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-04-14 00:01:35 +0530

    Added clipPath text to the About page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import ClipText from "./ClipText"
 
 export default function AboutMe() {
+    const textOne = "I am a self-taught creative web developer ";
+    const textTwo = "based in Hyderabad, India. I am passionate ";
+    const textThree = "about crafting my work for companies that ";
+    const textFour = " make an impact, inspire, and connect with people.";
     return (
         <div className='px-10 flex justify-center align-middle flex-col w-11/12 m-auto h-screen'>
-            <h2 className='c-100 px-10 uppercase text-2xl font-medium'>About me</h2>
-            <p className='c-400 line-h-ex2 px-10 pt-5 text-6xl font-medium'>I am a self-taught creative web developer based in Hyderabad, India. I am passionate about crafting my work for companies that make an impact, inspire, and connect with people.</p>
+            <h2 className='c-100 px-10 uppercase text-2xl pb-5 font-medium'>About me</h2>
+            <ClipText text={textOne} />
+            <ClipText text={textTwo} />
+            <ClipText text={textThree} />
+            <ClipText text={textFour} />
         </div>
     )
 }
