18ab3ffa2e682bad228eed967e7a7293a49621dc
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-04-13 12:55:42 +0530

    changed color in the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <div className='landing b-200 flex justify-center items-center h-screen w-11/12 m-auto flex-col relative'>
             <div className='box'>
-                <span className='c-200 uppercase text-8xl font-semibold text-start'>Hello there, I'm</span>
+                <span className='c-100 uppercase text-8xl font-semibold text-start'>Hello there, I'm</span>
                 <h1 className='c-100 pt-10 uppercase text-8xl font-semibold text-end'>Srivardhan</h1>
                 {/* <div>
                     <p className='c-300 pt-3 uppercase  font-medium text-end'>STUDENT AND A WEB DEVELOPER</p>
