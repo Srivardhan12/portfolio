fbe068facdf901aff584fcc28e9b620ae169f960
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-06-06 12:40:45 +0530

    made some redsponsive changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Main() {
     return (
-        <div className='landing b-200 flex justify-center items-center h-screen w-11/12 m-auto flex-col relative'>
+        <div className='landing b-200 flex justify-center items-center h-screen w-full m-auto flex-col relative'>
             <div className='box'>
                 <span className='c-100 uppercase text-8xl font-semibold text-start'>Hello there, I'm</span>
                 <h1 className='c-100 pt-10 uppercase text-8xl font-semibold text-end'>Srivardhan</h1>
