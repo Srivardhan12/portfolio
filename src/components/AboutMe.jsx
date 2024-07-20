6abe5984008bc92fd9e4007b31ec4cd55a1ff982
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-07-20 10:01:51 +0530

    made about me page responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import ClipText from "./ClipText"
+import React from "react";
+import ClipText from "./ClipText";
 
 export default function AboutMe() {
-    // const textOne = "I'm a self-taught creative web developer ";
-    // const textTwo = "based in Hyderabad, India. I am passionate ";
-    // const textThree = "about crafting my work for companies that ";
-    // const textFour = "make an impact, inspire, and connect with people.";
-    const aboutMePara = ["I'm a self-taught creative web developer ", "based in Hyderabad, India. I am passionate ", "about crafting my work for companies that ", "make an impact, inspire, and connect with people."]
-    return (
-        <div className='px-10 flex justify-center align-middle flex-col w-11/12 m-auto min-h-screen'>
-            <h2 className='c-100 px-10 uppercase text-base pb-5 font-semibold tracking-widest'>About me</h2>
-            {aboutMePara.map((el) => {
-                return <ClipText key={el} text={el} end={75} />
-            })}
-        </div>
-    )
+  // const textOne = "I'm a self-taught creative web developer ";
+  // const textTwo = "based in Hyderabad, India. I am passionate ";
+  // const textThree = "about crafting my work for companies that ";
+  // const textFour = "make an impact, inspire, and connect with people.";
+  const aboutMePara = [
+    "I'm a self-taught creative web developer ",
+    "based in Hyderabad, India. I am passionate ",
+    "about crafting my work for companies that ",
+    "make an impact, inspire, and connect with people.",
+  ];
+  return (
+    <div className="px-10 flex justify-center align-middle flex-col w-11/12 m-auto min-h-screen">
+      <h2 className="c-100 px-10 uppercase text-base pb-5 font-semibold tracking-widest md:pt-28">
+        About me
+      </h2>
+      {aboutMePara.map((el) => {
+        return <ClipText key={el} text={el} end={75} />;
+      })}
+    </div>
+  );
 }
