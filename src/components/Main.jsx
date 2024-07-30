0f70bcd4bbb607e883c9a3a2501857299ab009f2
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-07-31 01:22:15 +0530

    fixed main page alignment

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Main() {
   return (
     <div className="landing b-200 flex justify-center items-center h-screen w-full m-auto flex-col relative">
-      <div className="pt-32">
+      <div className="pt-48">
         <p className="c-100 uppercase lg:text-8xl md:text-7xl text-4xl font-semibold text-center md:lg:text-center md:mr-24">
           Hello there, I'm
         </p>
@@ -12,11 +12,9 @@ export default function Main() {
         </h1>
       </div>
       <div>
-        {/* <div className='absolute mt-96 rotate-90'> */}
         <p className="mt-40 bounce-ball animate-bounce w-10 h-10 b-100 lg:md:mx-auto c-200 text-center">
           &darr;
         </p>
-        {/* <p className='bounce-ball animate-bounce mt-40 text-base w-auto rotate-90' style={{ "fontSize": "16px" }}>Scroll down</p> */}
       </div>
     </div>
   );
