816b8f47bf35a05578ede078c43d823a45c39835
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-07-31 21:55:54 +0530

    fixed the about me for mobile view

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,8 +6,8 @@ export default function AboutMe() {
   // const textTwo = "based in Hyderabad, India. I am passionate ";
   // const textThree = "about crafting my work for companies that ";
   // const textFour = "make an impact, inspire, and connect with people.";
-  // const aboutMePara =
-  //   "I'm a self-taught creative web developer based in Hyderabad, India. I am passionate about crafting my work for companies that make an impact, inspire, and connect with people.";
+  const aboutMeParaSM =
+    "I'm a self-taught creative web developer based in Hyderabad, India. I am passionate about crafting my work for companies that make an impact, inspire, and connect with people.";
   const aboutMePara = [
     "I'm a self-taught creative web developer ",
     "based in Hyderabad, India. I am passionate ",
@@ -20,8 +20,7 @@ export default function AboutMe() {
         About me
       </h2>
       <p className="c-400 md:lg:line-h-ex leading-10 md:lg:px-10 text-3xl lg:text-6xl font-semibold sm:hidden">
-        I create purposeful digital experiences. Passionate about coding,
-        crafting sleek designs, and bringing ideas to life interactively.
+        {aboutMeParaSM}
       </p>
       {/* {textParas.map((el) => {
           return <ClipText key={el} text={el} end={75} />;
