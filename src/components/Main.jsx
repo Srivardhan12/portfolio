8b11b3d6927a4978c44b93406f33d9353788057f
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-07-31 21:39:55 +0530

    changed the color in the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div className="landing b-200 flex justify-center items-center h-screen w-full m-auto flex-col relative">
+    <div className="landing b-300 flex justify-center items-center h-screen w-full m-auto flex-col relative">
       <div className="pt-48">
         <p className="c-100 uppercase lg:text-8xl md:text-7xl text-4xl font-semibold text-center md:lg:text-center md:mr-24">
           Hello there, I'm
