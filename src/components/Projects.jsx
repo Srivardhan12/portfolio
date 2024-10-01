629b008f26741eb1ec352d73e24ddf62f8dd6417
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-10-01 11:21:00 +0530

    Changed scrool bar style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,8 +29,8 @@ export default function Projects() {
       </h2>
       {porjects.map((p) => {
         return (
-          <div className="md:lg:px-8 pt-10 flex lg:flex-row flex-col border-b border-slate-200 pb-20">
-            <Link to={p.liveDemoURL} target="_blank">
+          <div className="pt-10 flex lg:flex-row flex-col border-b border-slate-200 pb-20">
+            <Link to={p.liveDemoURL} target="_blank" className="bg-gray-300">
               <img
                 src={p.projectImage}
                 className="rounded-md hover:scale-105 transition md:lg:w-8/12 sm:w-full"
