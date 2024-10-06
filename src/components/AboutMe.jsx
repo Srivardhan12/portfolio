a770dd5ba26cd111cf8daa37b4712785163b985f
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2024-10-06 14:05:09 +0530

    Fixed the text line-height issue

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,35 +1,16 @@
 import React from "react";
-// import ClipText from "./ClipText";
 
 export default function AboutMe() {
-  // const textOne = "I'm a self-taught creative web developer ";
-  // const textTwo = "based in Hyderabad, India. I am passionate ";
-  // const textThree = "about crafting my work for companies that ";
-  // const textFour = "make an impact, inspire, and connect with people.";
-  const aboutMeParaSM =
-    "I'm a self-taught creative web developer based in Hyderabad, India. I am passionate about crafting my work for companies that make an impact, inspire, and connect with people.";
-  const aboutMePara = [
-    "I'm a self-taught creative web developer ",
-    "based in Hyderabad, India. I am passionate ",
-    "about crafting my work for companies that ",
-    "make an impact, inspire, and connect with people.",
-  ];
   return (
     <div className="md:lg:px-10 flex justify-center align-middle flex-col w-11/12 m-auto md:lg:min-h-screen sm:h-fit">
       <h2 className="c-100 md:lg:px-10 uppercase text-base pb-5 font-semibold tracking-widest md:pt-28 pt-16">
         About me
       </h2>
-      <p className="c-400 line-h-ex sm:leading-snug leading-10 md:lg:px-10 text-3xl lg:text-6xl font-semibold sm:hidden">
-        {aboutMeParaSM}
+      <p className="c-400 md:lg:px-10 text-3xl lg:text-6xl/[5rem] font-semibold custom-line-height">
+        I'm a self-taught creative web developer based in Hyderabad, India. I am
+        passionate about crafting my work for companies that make an impact,
+        inspire, and connect with people.
       </p>
-      {/* {textParas.map((el) => {
-          return <ClipText key={el} text={el} end={75} />;
-        })} */}
-      {/* <div className="c-400 line-h-ex sm:leading-10 md:lg:px-10 px-0 text-3xl lg:text-6xl font-semibold hidden md:lg:block">
-        {aboutMePara.map((el) => {
-          return <p className="leading-snug">{el}</p>;
-        })}
-      </div> */}
     </div>
   );
 }
--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -2,26 +2,15 @@ import React from "react";
 // import ClipText from "./ClipText";
 
 export default function WhatDoIDo() {
-  // const textForSm =
-  //   "I create purposeful digital experiences. Passionate about coding, crafting sleek designs, and bringing ideas to life interactively.";
-  // const splaitedText = textForSm.split(" ");
-  const textParas = [
-    "I create purposeful digital experiences. ",
-    "Passionate about coding, crafting sleek ",
-    "designs, and bringing ideas to life interactively.",
-  ];
   return (
     <div className="md:lg:px-10 b-100 flex md:justify-center align-middle flex-col w-11/12 m-auto md:lg:min-h-screen h-fit">
       <h2 className="c-100 md:lg:px-10 uppercase pb-5 text-base font-semibold tracking-widest md:lg:pt-20 pt-10">
         What i do?
       </h2>
-      <p className="c-400 line-h-ex lg:leading-snug md:lg:px-10 text-3xl lg:text-6xl font-semibold">
+      <p className="c-400 md:lg:px-10 text-3xl lg:text-6xl/[5rem] font-semibold custom-line-height">
         I create purposeful digital experiences. Passionate about coding,
         crafting sleek designs, and bringing ideas to life interactively.
       </p>
-      {/* {textParas.map((el) => {
-          return <ClipText key={el} text={el} end={75} />;
-        })} */}
     </div>
   );
 }
